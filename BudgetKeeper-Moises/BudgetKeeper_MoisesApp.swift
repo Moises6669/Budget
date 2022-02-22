38041f504d0204e2937172d0d7862c35ef40d003
Moises6669 <moisesalex132@gmail.com>
2022-02-22 13:54:12 -0700

    Initial Commit

---
+++ BudgetKeeper-Moises/BudgetKeeper_MoisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetKeeper_MoisesApp.swift
+//  BudgetKeeper-Moises
+//
+//  Created by Moises on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct BudgetKeeper_MoisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
